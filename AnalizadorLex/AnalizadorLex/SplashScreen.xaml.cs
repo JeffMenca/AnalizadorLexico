5c988de7c3b8f5ccee3a49d31686cc4b3095bbf2
JeffMenca <jeffrey.menendez.castillo@gmail.com>
2020-08-18 23:03:19 -0600

    SplashScreen

--- AnalizadorLex/AnalizadorLex/SplashScreen.xaml.cs
+++ AnalizadorLex/AnalizadorLex/SplashScreen.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Threading;
 
 namespace AnalizadorLex
 {
@@ -19,9 +21,43 @@ namespace AnalizadorLex
     /// </summary>
     public partial class SplashScreen : Window
     {
+        BackgroundWorker bw = new BackgroundWorker();
         public SplashScreen()
         {
             InitializeComponent();
         }
+
+
+        private void Bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            
+                MainWindow mn = new MainWindow();
+                mn.Show();
+                Close();
+        }
+
+        private void Bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pb_loading.Value = e.ProgressPercentage;
+        }
+
+        private void Bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(30);
+                bw.ReportProgress(i);
+            }
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            bw.WorkerReportsProgress = true;
+            bw.DoWork += Bw_DoWork;
+            bw.ProgressChanged += Bw_ProgressChanged;
+            bw.RunWorkerCompleted += Bw_RunWorkerCompleted;
+            bw.RunWorkerAsync();
+        }
+
     }
 }
